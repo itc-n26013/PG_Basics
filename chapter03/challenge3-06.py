cb54c8bd98fec01840135189767cd08983c03050
itc-n26013 <n26013@std.it-college.ac.jp>
2026-05-28 07:17:17 +0000

    第3章演習終了

--- chapter03/challenge3-06.py
+++ chapter03/challenge3-06.py
@@ -1,5 +1,9 @@
-age = "False"
-if age=="True":
- print("ageはTrueっす！")
+age = "令和"
+if age=="平成":
+ print("ageは平成っす！")
+elif age=="昭和":
+    print("ageは昭和っす！")
+elif age=="明治":
+    print("ageは明治っす！")
 else:
- print("ageはFalseっす！")
+ print("ageは令和っす！")
