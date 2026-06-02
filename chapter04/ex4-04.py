75ccdcb7cc777b8e4b5ce58136cd00fdc67b25bb
itc-n26013 <n26013@std.it-college.ac.jp>
2026-06-02 07:52:26 +0000

    第4章　例題22まで

---
+++ chapter04/ex4-04.py
@@ -0,0 +1,6 @@
+def f(x,y,z):  
+    return x+y+z
+
+result = f(1,2,3)　#変数を呼び出した結果をResultに代入
+print(result)
+
---
+++ chapter04/ex4-10.py
@@ -0,0 +1,24 @@
+n= input("type a number:")
+n= int(n)
+if n % 2 == 0:
+    print("n id even.")
+else:
+    print("n is odd.")
+n= input("type a number:")
+n= int(n)
+if n % 2 == 0:
+    print("n id even.")
+else:
+    print("n is odd.")
+n= input("type a number:")
+n= int(n)
+if n % 2 == 0:
+    print("n id even.")
+else:
+    print("n is odd.")
+n= input("type a number:")
+n= int(n)
+if n % 2 == 0:
+    print("n id even.")
+else:
+    print("n is odd.")
