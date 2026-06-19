2bd614d428d76a837bb16662a845a06f2d2841cc
itc-n26013 <n26013@std.it-college.ac.jp>
2026-06-19 12:38:25 +0000

    第4章　演習問題終了

--- chapter04/ex4-10.py
+++ chapter04/ex4-10.py
@@ -1,24 +1,24 @@
 n= input("type a number:")
 n= int(n)
 if n % 2 == 0:
-    print("n id even.")
+    print("n is even.")
 else:
     print("n is odd.")
 n= input("type a number:")
 n= int(n)
 if n % 2 == 0:
-    print("n id even.")
+    print("n is even.")
 else:
     print("n is odd.")
 n= input("type a number:")
 n= int(n)
 if n % 2 == 0:
-    print("n id even.")
+    print("n is even.")
 else:
     print("n is odd.")
 n= input("type a number:")
 n= int(n)
 if n % 2 == 0:
-    print("n id even.")
+    print("n is even.")
 else:
     print("n is odd.")
